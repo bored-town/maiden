a38afae5bd4ca3cb014dff9ef6eeaa77faa9de6d
diewland <diewland@gmail.com>
2025-03-08 14:13:49 +0700

    update render image script

--- scripts/2_gen_img_from_json.py
+++ scripts/2_gen_img_from_json.py
@@ -1,12 +1,21 @@
+from pprint import pprint as pp
 from common import *
-from 1_gen_json_from_traits import trait_paths
+import json
 
-# gen images
-chars = [
-    (
-        trait_paths[0][1].split('*')[0] + a,
-        trait_paths[1][1].split('*')[0] + b,
-        trait_paths[2][1].split('*')[0] + c,
-    ) for (a, b, c) in data
-]
-save_img(chars, './out/{}.png')
+chars     = []
+json_path = '../docs/{}.json'
+out_path  = './out/{}.png'
+
+# craft chars from json
+for token_id in range(1, 10_000+1):
+    src = json_path.format(token_id)
+    data = json.load(open(src))
+    (_agent, _mandala, _planet) = data['attributes']
+    chars.append((
+        '../traits/1_Planet/{}.jpg'.format(_planet['value']),
+        '../traits/2_Mandala/{}.png'.format(_mandala['value']),
+        '../traits/3_Agent/{}.png'.format(_agent['value']),
+    ))
+
+# render images
+save_img(chars, out_path)
